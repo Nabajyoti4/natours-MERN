9da8fb7cd7b483375bd80edee5d55440957c9f46
Nabajyoti4 <sirnaba@gmail.com>
2021-08-19 16:22:57 +0530

    error handler for uncaught and unhandles errors added

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const dotenv = require('dotenv');
 
+  // uncaught exceptions
+  process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION')
+
+      process.exit(1)
+
+  }) 
+
+
 dotenv.config({ path: './config.env' });
+
 
 const mongoose = require('mongoose');
 const app = require('./app');
@@ -21,7 +32,21 @@ mongoose
   })
   .then(() => {
     console.log('DB connection successfull');
-    app.listen(port, () => {
-      console.log(`App running on ${port}`);
-    });
+
   });
+
+  const server = app.listen(port, () => {
+    console.log(`App running on ${port}`);
+  });
+
+
+  //handle unhandles rejection
+  process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION')
+    server.close(() => {
+      process.exit(1)
+    });
+  })
+
+
